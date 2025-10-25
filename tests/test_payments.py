e861855fec6633d98a1076d418140d31d1894b92
matiascaccia <matiascacciag@gmail.com>
2025-10-25 12:51:40 -0300

    Add parent to test

--- tests/test_payments.py
+++ tests/test_payments.py
@@ -1,4 +1,8 @@
 from fastapi.testclient import TestClient
+import os
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from main import app, payments
 
 client = TestClient(app)
